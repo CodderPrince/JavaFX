e558bbdd06de43d5c177148b423db07ceea485e2
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-11 00:32:58 +0600

    Create Demo.java

---
+++ JavaFx/Interface2/src/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+int []arr={2,3,45};
+for(int i=0;i<arr.length;i++){
+    System.out.println(arr[i]);
+}
+        System.out.println();
+    }
+}
