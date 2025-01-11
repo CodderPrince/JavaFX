f8a45570ba0076d03b96c443f9e0887e8daa3866
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-12 00:05:59 +0600

    Create RulesController.java

---
+++ JavaFx/Tic-Tac-Toe-Java-master/Tic-Tac-Toe-Java-master/src/main/java/controllers/RulesController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class RulesController {
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    public void closeRules() {
+        // Fermer la fenêtre modale
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+}
