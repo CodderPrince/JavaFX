5509f7163407073db664e248f7ee2fa9dd3658ab
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-12 00:08:37 +0600

    Create p41sumofseries2.c

---
+++ VS code/Dimik OJ/Dimik OJ ACCEPTED/p41sumofseries2.c
@@ -0,0 +1,25 @@
+ #include<stdio.h>
+ long long int fact(long long int n){
+	long long int product=1,i;
+    for(i=n;i>=1;i--){
+    	product*=i;
+	}
+	return product;
+}
+
+int main()
+{
+	int t;
+	long long int n;
+	double sum=0.0;
+	scanf("%d",&t);
+
+	while(t--){
+		scanf("%lld", &n);
+		sum=0.0;
+		for(int j=1;j<=n;j++){
+			sum+=(double)j/fact(j);
+		}
+		printf("%0.4lf\n",sum);
+	}
+}
