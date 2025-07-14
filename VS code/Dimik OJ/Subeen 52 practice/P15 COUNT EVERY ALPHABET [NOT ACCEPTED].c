55c61bf065befbc39ce718c14a964ef91755ffae
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-15 00:55:49 +0600

    Create P15 COUNT EVERY ALPHABET [NOT ACCEPTED].c

---
+++ VS code/Dimik OJ/Subeen 52 practice/P15 COUNT EVERY ALPHABET [NOT ACCEPTED].c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str [1002];
+        int i, count[26];
+        scanf(" %[^\n]",str);
+        for(i=0; i<26; i++)
+        {
+            count[i]=0;
+        }
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]>='a' && str[i]<='z')
+            {
+                count[str[i]-'a']++;
+
+            }
+        }
+            for(i=0; i<26; i++)
+            {
+                if(count[i]!=0)
+                {
+                    printf("%c = %d\n", 'a'+i, count[i]);
+                }
+            }
+            if(t>0)
+            {
+                printf("\n");
+            }
+
+        }
+    return 0;
+}
+
