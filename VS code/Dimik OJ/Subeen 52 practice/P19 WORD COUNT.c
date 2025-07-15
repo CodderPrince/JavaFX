a1170b41874b684b0e1bff3422f6ccc8a45908ea
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-16 00:04:31 +0600

    Create P19 WORD COUNT.c

---
+++ VS code/Dimik OJ/Subeen 52 practice/P19 WORD COUNT.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int t;
+scanf("%d", &t);
+getchar();
+while(t--){
+char str[10001];
+gets(str);
+int count=0;
+char*token= strtok(str, " ");
+while(token!=0)
+{
+    count++;
+    token= strtok(0," ");
+}
+printf("Count = %d\n", count);
+
+}
+return 0;
+}
