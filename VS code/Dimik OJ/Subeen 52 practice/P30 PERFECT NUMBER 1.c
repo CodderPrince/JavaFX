f24f48a8b8542c9e92b7504cfdb6bbe9be5d8a18
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-20 00:32:17 +0600

    Create P30 PERFECT NUMBER 1.c

---
+++ VS code/Dimik OJ/Subeen 52 practice/P30 PERFECT NUMBER 1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	unsigned long int n,a,i,sum=0,j,x,m;
+	int t;
+	scanf("%d", &t);
+	for(i=0;i<t;i++)
+        {
+		scanf("%llu", &n);
+		sum=0;
+		//m=n;
+		for(j=1;j<n;j++){
+			if((n%j)==0){
+				sum = sum+j;
+			}
+		}
+		if(sum==n)
+		{
+			printf("YES, %llu is a perfect number!\n",n);
+		}
+		else
+		{
+		   printf("NO, %llu is not a perfect number!\n",n);
+		}
+
+	}
+}
