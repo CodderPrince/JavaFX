1bfa4d0c17d85f1b22aa942fbb0b5f13e7ef51e3
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-21 23:42:49 +0600

    Create P35 OUTSIDE THE CIRCLE.c

---
+++ VS code/Dimik OJ/Subeen 52 practice/P35 OUTSIDE THE CIRCLE.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int t,n,r,x1,x2,y1,y2,i,j,a,b,l,k;
+	scanf("%d",&t);
+	while(t--){
+
+		scanf("%d %d %d %d %d", &x1,&y1, &r, &x2,  &y2);
+		a=(x2-x1)*(x2-x1);
+		b=(y2-y1)*(y2-y1);
+		l=sqrt(a+b);
+		if(l>r){
+
+			printf("The point is not inside the circle\n");
+		}
+		else
+		{
+
+		printf("The point is inside the circle\n");
+		}
+
+	//printf("\n");
+	}
+	return 0;
+}
