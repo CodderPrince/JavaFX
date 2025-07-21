c610bb5189693b26ce7d07f8e881971e2f9a1e11
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-21 23:42:53 +0600

    Create P36.c

---
+++ VS code/Dimik OJ/Subeen 52 practice/P36.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int i,n,string;
+    scanf("%d", &n);
+		for(i=1;i<=n;i++)
+	char string[100];
+  printf("\n\t Enter the string : ");
+	scanf("%s",string);
+	char temp;
+	int i, j;
+	int n = strlen(string);
+	for (i = 0; i < n-1; i++) {
+		for (j = i+1; j < n; j++) {
+			if (string[i] > string[j]) {
+					temp = string[i];
+					string[i] = string[j];
+					string[j] = temp;
+			}
+		}
+	}
+    }
+
+	printf("\n");
+	return 0;
+}
