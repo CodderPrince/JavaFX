1484e0854f1c3d92a65ff96b7ef99a354630e3b6
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-25 00:01:37 +0600

    Create 19wordcount1.c

---
+++ VS code/Dimik OJ/SUBEEN 52 PROBLEM SOLUTION/Selected problem solution most easiest way/19wordcount1.c
@@ -0,0 +1,33 @@
+/******************************************************
+ *    Author:  PRINCE_14                              *
+ *          --MD. AN NAHIAN PRINCE                    *
+ *          --CSE,BRUR                                *
+ *          --FB: https://facebook.com/mdannahian     *
+ *            PB: Word Count 1 [19]                   *
+ ******************************************************/
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        int count=0,i;
+        char str[100001];
+        gets(str);
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i] == ' ')
+            {
+                count++;
+            }
+        }
+        printf("Count = %d\n",count+1);
+    }
+
+    return 0;
+}
